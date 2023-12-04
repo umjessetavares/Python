6f87967a6300516758ede3f53f9f4e181a48b590
Jesse <jessetavarescardoso@gmail.com>
2023-12-04 16:46:59 -0300

    sync

--- Udemy/start.py
+++ Udemy/start.py
@@ -1,1 +1,6 @@
-print('Hello')
+print('Hello World')
+print('Meu nome é Jesse')
+
+print('Eu tenho apenas' + '36 anos')
+print('Eu tenho apenas ' + '36 anos')
+print('Eu tenho apenas' + ' ' + '36 anos')
