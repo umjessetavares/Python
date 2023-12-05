93298e0b05f8a9e5a3c9e45946c6f7b0b8770a50
Jesse <jessetavarescardoso@gmail.com>
2023-12-04 21:09:26 -0300

    Changes to be committed:     new file:   Udemy/calculadora.py

---
+++ Udemy/calculadora.py
@@ -0,0 +1,11 @@
+# SOMA
+num1 = input('Digite o primero número: ')
+num2 = input('Digite o segundo número: ')
+# print(num1 + num2)
+
+# int precisa ser base10
+#resultado = int(num1) + int(num2)
+
+# float aceita decimais
+resultado = float(num1) + float(num2)
+print(resultado)
